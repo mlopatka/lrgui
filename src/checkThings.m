82a6252fbceda8362c4a40e72a554fb57c8352eb
doomling <martin.lopatka@gmail.com>
2017-01-10 13:13:18 -0800

    updated to include p files

--- src/checkThings.m
+++
@@ -1,8 +0,0 @@
-function wegood = checkThings
-VALID_TILL = '31-Dec-2016';
-today = datenum(date);
-if today > datenum(VALID_TILL)
-    wegood = false;
-else
-    wegood = true;
-end
---
+++ src/m/checkThings.m
@@ -0,0 +1,8 @@
+function wegood = checkThings
+%VALID_TILL = '31-Dec-2016';
+%today = datenum(date);
+%if today > datenum(VALID_TILL)
+%    wegood = false;
+%else
+    wegood = true;
+end
